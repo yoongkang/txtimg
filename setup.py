962ad6b8bf604152ffb74e3db18ba5acbf521580
Yoong Kang Lim <yoongkang.lim@gmail.com>
2020-06-21 21:28:37 +1000

    Bump txtimg version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="txtimg",
-    version="0.0.2",
+    version="0.0.3",
     author="Yoong Kang Lim",
     author_email="yoongkang.lim@gmail.com",
     description="A library to easily create text-based images (e.g. images that primarily contain text).",
